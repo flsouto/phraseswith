89c595c712e27f878fc1d0a89bb074bde0f133af
Fabio Souto <fabiolimasouto@gmail.com>
2016-11-20 15:17:45 -0200

    refactor parser to process each book in a separate process to avoid memory overflow

--- parse.php
+++ parse.php
@@ -14,132 +14,15 @@ while(!empty($books)){
 
 	$file = array_shift($books);
 	
-	$contents = file_get_contents($file);
-	$contents = preg_replace("/\[Illustration:.*?\]/i","",$contents);
-
-	$source = rtrim(basename($file),'.txt');
-
-	preg_match_all("/\s([a-z]{3,})\s/",$contents,$matches,PREG_OFFSET_CAPTURE);
-
-	$grouped = [];
-
-	foreach($matches[1] as $word){
-		$grouped[$word[0]][] = $word[1];
-	}
-
-	foreach($grouped as $word => $positions){
-
-		$word_file = __DIR__."/words/$word.txt";
-		
-		$count = count($positions);
-		
-		if($count <= 50) {
-			$goal = $count;
-		} else if($count <= 100){
-			$goal = 50;
-		} else {
-			$goal = 1;
-		}
-
-		$phrases = 0;
-		shuffle($positions);
-		foreach($positions as $pos){
-			$phrase = extractPhrase($contents, $pos);
-			if(strlen($phrase)<30 || strlen($phrase)>200){
-				continue;
-			}
-			if(!preg_match("/^[A-Z]/",$phrase)){
-				continue;
-			}
-			if(strstr($phrase,'"')){
-				continue;
-			}
-			if(substr($phrase,-1)!='.'){
-				continue;
-			}
-			if(!file_exists($word_file)){
-				touch($word_file);
-			}
-			file_put_contents($word_file, $phrase.'|'.$source.PHP_EOL, FILE_APPEND);
-			$phrases++;
-			if($phrases>=$goal){
-				break;
-			}
-		}
-
-	}
+	shell_exec('php '.__DIR__.'/parse_book.php "'.$file.'"');	
 
 	echo "Remaining: ".count($books).PHP_EOL;
 
+	break;
+
 }
 
-function extractPhrase($content,$pos){
 
-	$length = strlen($content);
-
-	$append = '';
-	for($i=$pos;$i<$length;$i++){
-		$char = substr($content,$i,1);
-		$char_prev = substr($content,$i-1,1);
-		if($char==" "&&$char_prev==" "){
-			continue;
-		}
-		if(ctype_cntrl($char)){
-			if(ctype_cntrl($char_prev)){
-				break;
-			}
-			$char = " ";
-		}
-		$append .= $char;
-		if($char=="." && checkShouldBreak($content,$i)){
-			break;
-		}
-	}
-
-	$prepend = '';
-	for($i=$pos-1;$i>=0;$i--){
-		$char = substr($content,$i,1);
-		$char_prev = substr($content,$i+1,1);
-
-		if($char==" "&&$char_prev==" "){
-			continue;
-		}
-		if($char=='.' && checkShouldBreak($content,$i)){
-			break;
-		}
-		if(ctype_cntrl($char)){
-			if(ctype_cntrl($char_prev)){
-				break;
-			}
-			$char = " ";
-		}
-		$prepend = $char.$prepend;
-	}
-
-	$phrase = ltrim($prepend).$append;
-    return $phrase;
-}
-
-function checkShouldBreak($content, $period_pos){
-	$word = "";
-	for($i=$period_pos-1;$i>=0;$i--){
-		$char = substr($content,$i,1);
-		if(!preg_match("/[a-z]/i",$char)){
-			break;
-		}
-		$word = $char.$word;
-	}
-
-	if(preg_match("/^[A-Z]/",$word)){
-		return false;
-	}
-
-	if(in_array(strtolower($word),['mr','sr','mrs','srs'])){
-		return false;		
-	}
-
-	return true;
-}
 
 
 
